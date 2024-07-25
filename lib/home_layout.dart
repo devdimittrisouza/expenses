4948fa9b9cba9211f55f5c830675e9b242f47447
Dimittri Souza <dimittrisouza@gmail.com>
2024-07-25 16:46:22 -0300

    Adição de formulário de nova transação e lógica pra recuperar o que foi digitado através de controllers.

--- lib/home_layout.dart
+++ lib/home_layout.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/transactions.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomeState extends StatefulWidget {
   const HomeState({super.key});
@@ -9,6 +10,10 @@ class HomeState extends StatefulWidget {
 }
 
 class _HomeStateState extends State<HomeState> {
+
+  final _valueController = TextEditingController();
+  final _titleController = TextEditingController();
+
   final _transactions = [
     Transactions(
         id: 't1',
@@ -25,7 +30,6 @@ class _HomeStateState extends State<HomeState> {
       appBar: AppBar(
           title: Text("Despesas pessoais"), backgroundColor: Colors.lightBlue),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
@@ -69,7 +73,7 @@ class _HomeStateState extends State<HomeState> {
                           ),
                         ),
                         Text(
-                          tr.date.toString(),
+                          DateFormat('d MMM y').format(tr.date),
                           style: TextStyle(fontSize: 16,
                               color: Colors.grey),
                         )
@@ -79,6 +83,46 @@ class _HomeStateState extends State<HomeState> {
                 ),
               );
             }).toList(),
+          ),
+          Card(
+            elevation: 5,
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _titleController,
+                    decoration: InputDecoration(
+                      labelText: 'Título'
+                    ),
+                  ),
+                  TextField(
+                    controller: _valueController,
+                    decoration: InputDecoration(
+                        labelText: 'Valor (R\$)'
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        child: Text("Nova transação"),
+                        style: TextButton.styleFrom(
+                          textStyle: TextStyle(
+                            color: Colors.purple,
+                            fontWeight: FontWeight.bold
+                          )
+                        ),
+                        onPressed: (){
+                          print(_titleController.text);
+                          print(_valueController.text);
+                        },
+                      )
+                    ],
+                  )
+                ],
+              )
+            ),
           )
         ],
       ),
